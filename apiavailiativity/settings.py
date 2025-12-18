6985fa34e997ce730b350f10935ef913dff6ec14
bondanocho123 <bondanocho123@gmail.com>
2025-12-18 16:32:24 +0700

    Update settings.py

--- apiavailiativity/settings.py
+++ apiavailiativity/settings.py
@@ -70,9 +70,14 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
-
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
