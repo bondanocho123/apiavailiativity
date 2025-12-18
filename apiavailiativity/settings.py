daab0ea88363e9dbd8457da1a862a172274b7ac2
bondanocho123 <bondanocho123@gmail.com>
2025-12-18 16:23:45 +0700

    Update settings.py

--- apiavailiativity/settings.py
+++ apiavailiativity/settings.py
@@ -20,7 +20,11 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
 
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
